abdecc5f7d55e804598fa6ad2b820aa967322612
Bastien Abadie <bastien.abadie@gmail.com>
2014-12-03 15:50:55 +0100

    Add task to build minified distribution.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,14 @@ module.exports = function(grunt) {
         autoIndex: true,
         ext: "html"
       },
+      dist: {
+        port: 8200,
+        root : 'dist',
+        host: "127.0.0.1",
+        showDir : true,
+        autoIndex: true,
+        ext: "html"
+      },
     },
 
     concat: {
@@ -57,17 +65,73 @@ module.exports = function(grunt) {
         },
       },
     },
+
+    // Build workflow for distribution
+    clean : {
+      dist : ['dist, '],
+    },
+    copy : {
+      dist : {
+        files : [
+          {
+            src : 'app/index.html',
+            dest : 'dist/index.html',
+          },
+          {
+            expand : true,
+            cwd : 'app/templates',
+            src : ['**.html', ],
+            dest : 'dist/templates'
+          },
+          {
+            expand : true,
+            cwd : 'medias/img',
+            src : ['**.png', '**.jpg', '**.gif', ],
+            dest : 'dist/medias/img'
+          },
+        ],
+      },
+    },
+    useminPrepare : {
+      src : ['app/index.html', ],
+      options : {
+        root : '.',
+      },
+    //  dest : 'dist',
+    },
+    usemin : {
+      html : 'dist/index.html',
+    },
   });
 
   grunt.loadNpmTasks('grunt-http-server');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Custom tasks
+  // Dev default task
   grunt.registerTask('default', [
     'concat', // build vendors assets
     'less:dev', // build our style
     'http-server:dev', // serve our app & medias
+  ]);
+
+  // Build distribution
+  grunt.registerTask('build', [
+    'clean:dist',
+    'concat:vendor_js', // build base vendors
+    'concat:vendor_css',
+    'less:dev',
+    'copy:dist', // Copy base html & medias
+    'useminPrepare', // Detect files to minify & uglify
+    'concat:generated',
+    'cssmin:generated',
+    'uglify:generated',
+    'usemin', // Apply modifications to dist html
   ]);
 };
